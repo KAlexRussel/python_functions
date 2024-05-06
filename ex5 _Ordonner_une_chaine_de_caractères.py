0771ecfda3d225f24d4eadb2d525aa5aeb3b929a
KAlexRussel <kouawoualex1234@gmail.com>
2024-05-06 12:20:14 +0100

    exercice 5 lower letters
    Signed-off-by: KAlexRussel <kouawoualex1234@gmail.com>

--- ex5 _Ordonner_une_chaine_de_caractères.py
+++ ex5 _Ordonner_une_chaine_de_caractères.py
@@ -5,10 +5,10 @@ def ordonne_str(st):
     lettre_alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     new_str = ""
     for i in range(len(lettre_alphabet)):
-        if lettre_alphabet[i] in st:
+        if lettre_alphabet[i] in st.lower():
             new_str = new_str + lettre_alphabet[i]
     print(new_str)
     
     return new_str
 
-ordonne_str('python')
+ordonne_str('Python')
